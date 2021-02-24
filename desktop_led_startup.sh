75b18e8311e969f18225e8661929bb2a71fcac78
Tony Duco <nosduco@gmail.com>
2021-02-23 21:56:01 -0500

    completed desktop rgb script

--- desktop_led_startup.sh
+++ desktop_led_startup.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-liquidctl set led color fixed ff2300
+COLOR=${1:-FF2300}
+echo $COLOR
+echo Setting colors for Corsair AIO Cooler
+liquidctl set led color fixed $COLOR
+echo Setting colors for Razer Mamba Elite Mouse
+openrgb --device $(openrgb --list-devices | grep "Razer Mamba Elite" | cut -c1-1) --mode static --color $COLOR
+echo Setting colors for MSI 2080 Super
+openrgb --device $(openrgb --list-devices | grep "MSI GeForce RTX 2080 Super Gaming X Trio" | cut -c1-1) --mode static --color $COLOR
+echo Done!
