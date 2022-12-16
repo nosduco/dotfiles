09e67645e6f05be412e2c6135a2b87d41cb46fef
Tony Duco <nosduco@gmail.com>
2022-12-16 18:01:38 -0500

    Reorganize and create archive of old files

--- old/scripts/desktop_led_startup.sh
+++ old/scripts/desktop_led_startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 COLOR=${1:-FF2300}
 echo Setting colors for Corsair AIO Cooler
-liquidctl set led color fixed $COLOR
-echo Setting colors for Razer Mamba Elite Mouse
-openrgb --device $(openrgb --list-devices | grep "Razer Mamba Elite" | cut -c1-1) --mode static --color $COLOR
+liquidctl --match hydro set led color fixed $COLOR
 echo Setting colors for MSI 2080 Super
 openrgb --device $(openrgb --list-devices | grep "MSI GeForce RTX 2080 Super Gaming X Trio" | cut -c1-1) --mode static --color $COLOR
+echo Setting colors for G703 Mouse
+openrgb --device $(openrgb --list-devices | grep "G703 LIGHTSPEED Wireless Gaming Mouse w/ HERO" | cut -c1-1) --mode static --color $COLOR
 echo Done!
