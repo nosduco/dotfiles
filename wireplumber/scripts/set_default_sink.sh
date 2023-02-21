fc862cc5dde8de8b11cb052b06ce268a8fa3c6c0
Tony Duco <nosduco@gmail.com>
2023-02-21 17:44:47 -0500

    add icons to notifications when switching between headphones/speakers via wireplumber

--- wireplumber/scripts/set_default_sink.sh
+++ wireplumber/scripts/set_default_sink.sh
@@ -5,10 +5,10 @@ if [ -z "$1" ]; then
   echo "Expected argument to be one of: [headphones, speakers]"
 elif [ $1 = "headphones" ]; then
   wpctl set-default $(wpctl status | grep -m 1 -A 2 "Sinks" | grep "Headphones" | sed 's/[^0-9]*\([0-9]*\).*/\1/')
-  notify-send "Headphones set as default output device."
+  notify-send --icon=/usr/share/icons/Adwaita/512x512/devices/audio-headphones.png "Headphones set as default output device."
 elif [ $1 = "speakers" ]; then
   wpctl set-default $(wpctl status | grep -m 1 -A 2 "Sinks" | grep "Speakers" | sed 's/[^0-9]*\([0-9]*\).*/\1/')
-  notify-send "Speakers set as default output device."
+  notify-send --icon=/usr/share/icons/Numix/64@2x/devices/audio-speakers.svg "Speakers set as default output device."
 fi
 
 
