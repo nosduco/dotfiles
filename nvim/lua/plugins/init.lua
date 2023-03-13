01434bd71283dae76a3b5d75f598c6405d114059
Tony Duco <nosduco@gmail.com>
2023-03-12 22:30:09 -0400

    add rust configs and rust-analyzer lsp to neovim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -25,10 +25,14 @@ return require('packer').startup(function(use)
   use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
 
   -- Extension Host/Language Server/Sitter
+  use 'neovim/nvim-lspconfig'
+  use 'simrat39/rust-tools.nvim'
   use {'neoclide/coc.nvim', branch = 'release'}
   use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use {'nathom/filetype.nvim'}
   use { 'jose-elias-alvarez/null-ls.nvim', requires = { {'nvim-lua/plenary.nvim'} } }
+  use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+  -- use 'mfussenegger/nvim-dap'
 
   -- Themes
   use { 'Shatur/neovim-ayu' }
