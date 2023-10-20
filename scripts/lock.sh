a0c1526178f7acc542c90eb11772432a86d97090
Tony Duco <nosduco@gmail.com>
2023-10-19 23:59:13 -0400

    feat!: all hyprland apps/configs + remove unused

---
+++ scripts/lock.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+swaylock --screenshots  \
+  --clock \
+  --indicator \
+  --indicator-radius 100 \
+  --indicator-thickness 7 \
+  --effect-blur 7x5 \
+  --effect-vignette 0.5:0.5 \
+  --ring-color 1f2430 \
+  --key-hl-color e95420 \
+  --line-color 00000000 \
+  --inside-color 00000088 \
+  --separator-color 00000000 \
+  --grace 2 \
+  --fade-in 0.2 \
+  -f
