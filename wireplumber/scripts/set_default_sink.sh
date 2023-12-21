a44e8689fbcfcea1e8c0f2ca73e43a61489027c9
Tony Duco <nosduco@gmail.com>
2023-12-21 18:11:38 -0500

    fix: desktop switch output wireplumber script

--- wireplumber/scripts/set_default_sink.sh
+++ wireplumber/scripts/set_default_sink.sh
@@ -4,7 +4,7 @@
 if [ -z "$1" ]; then
   echo "Expected argument to be one of: [headphones, speakers]"
 elif [ $1 = "headphones" ]; then
-  wpctl set-default $(wpctl status | grep -m 1 -A 6 "Sinks" | grep "Headphones" | sed 's/[^0-9]*\([0-9]*\).*/\1/')
+  wpctl set-default $(wpctl status | grep -m 1 -A 6 "Sinks" | grep "\. Headphones" | sed 's/[^0-9]*\([0-9]*\).*/\1/')
   notify-send -t 1000 -e --icon=/usr/share/icons/Adwaita/512x512/devices/audio-headphones.png "Headphones set as default output device."
 elif [ $1 = "speakers" ]; then
   wpctl set-default $(wpctl status | grep -m 1 -A 6 "Sinks" | grep "Speakers" | sed 's/[^0-9]*\([0-9]*\).*/\1/')
