d28db6a4467d2b061ff2f47b43d35e6a26413398
tony duco <td@tonydu.co>
2026-08-27 18:11:10 -0400

    fix: fish virtualenv

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,10 @@
-set -g VIRTUALFISH_VERSION 2.5.8
+set -g VIRTUALFISH_VERSION 2.5.9
 set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
-source /usr/lib/python3.13/site-packages/virtualfish/virtual.fish
-emit virtualfish_did_setup_plugins
+
+# Resolve virtualfish across Python minor-version bumps (3.13 -> 3.14 etc.)
+set -l _vf_candidates /usr/lib/python3.*/site-packages/virtualfish/virtual.fish
+if set -q _vf_candidates[1]
+    source $_vf_candidates[-1]
+    emit virtualfish_did_setup_plugins
+end
+set -e _vf_candidates
