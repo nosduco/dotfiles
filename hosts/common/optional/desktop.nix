3d15c0c8dac600c139a784c79ad7fc2a5f6270a5
tony duco <td@tonydu.co>
2026-09-09 19:50:58 -0400

    feat(nix): desktop base configuration

---
+++ hosts/common/optional/desktop.nix
@@ -0,0 +1,32 @@
+# Desktop base configuration
+{ lib, pkgs, ... }:
+{
+  # hyprland
+  programs.hyprland.enable = true;
+  programs.hyprland.withUWSM = true;
+
+  # greeter
+  services.greetd = {
+    enable = true;
+    useTextGreeter = true;
+    settings.default_session.user = "greeter";
+    # TODO: Change the name of the CMD to Hyprland
+    settings.default_session.command = "${lib.getExe' pkgs.tuigreet "tuigreet"} --time --remember --cmd 'uwsm start -e -D Hyprland hyprland.desktop'";
+  };
+
+  # audio
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # virtualization
+  virtualisation.vmVariant.virtualisation = {
+    memorySize = 4096;
+    cores = 4;
+    qemu.options = [ "-vga none -device virtio-gpu-pci" ];
+  };
+}
--- hosts/voyager/default.nix
+++ hosts/voyager/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ../common/core
+    ../common/optional/desktop.nix
   ];
 
   networking.hostName = "voyager";
